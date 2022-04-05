e839f6d860acf5c0cd98bd33e9d4f9c43e044dd0
Ivan Uresti <ivan.uresti@gmail.com>
2022-04-05 08:33:34 -0500

    add intentional error

--- src/main/java/uaslp/objetos/list/linkedlist/LinkedList.java
+++ src/main/java/uaslp/objetos/list/linkedlist/LinkedList.java
@@ -9,7 +9,7 @@ public class LinkedList <T> implements List <T> {
 
     private Node<T> head;
     private Node<T> tail;
-    private int size;
+    private int size = 1;
 
     public void addAtTail(T data) throws NotNullValuesAllowedException {
 
